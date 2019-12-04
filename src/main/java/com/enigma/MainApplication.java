8c117bb41a2d7ec4f95924ac2948d0351e7e4edb
drikalabs <andrika.bogal@gmail.com>
2019-12-04 16:55:26 +0700

    [FEATURE] able to execute jar file with file

--- src/main/java/com/enigma/MainApplication.java
+++ src/main/java/com/enigma/MainApplication.java
@@ -6,7 +6,12 @@ import com.enigma.process.FileProcessor;
 import com.enigma.process.FileProcessorImplementation;
 
 public class MainApplication {
+    private static final String ADDITIONAL_PATH="/commands-file/";
     public static void main(String[] args) {
-
+        System.out.println(args[0]);
+        String pathFile = System.getProperty("user.dir");
+        FileProcessor fileProcessor= new FileProcessorImplementation(pathFile+ADDITIONAL_PATH+args[0]);
+        CommandsProcessor commandsProcessor=new CommandsProcessorImplementation();
+        commandsProcessor.executeCommand(fileProcessor.readFile());
     }
 }
